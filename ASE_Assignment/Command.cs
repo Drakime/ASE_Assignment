ce9376e00fd4c575410d655817c16dbe053772fb
Hung Diep <quoc.hung.diep@outlook.com>
2022-10-11 16:42:50 +0100

    Create 'Command' and 'MoveTo' classes
    Create 'command' abstract class for other command-based
    classes to inherit from. Classes that inherit from 'command'
    implement abstract method 'operation', as well as common
    attributes.

---
+++ ASE_Assignment/Command.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASE_Assignment
+{
+    public abstract class Command
+    {
+        private string name = "";
+        private string userInput = "";
+        private List<int> parameterList = new List<int>();
+        protected Canvas drawingCanvas;
+        private Color toolColour = new Color();
+
+        public abstract void Operation();
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string UserInput
+        {
+            get { return userInput; }
+            set { userInput = value; }
+        }
+
+        public List<int> ParameterList
+        {
+            get { return parameterList; }
+            set { parameterList = value; }
+        }
+
+        protected Canvas DrawingCanvas
+        {
+            get { return drawingCanvas; }
+            set { drawingCanvas = value; }
+        }
+
+        private Color ToolColour
+        {
+            get { return toolColour; }
+            set { toolColour = value; }
+        }
+    }
+}
---
+++ ASE_Assignment/MoveTo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASE_Assignment
+{
+    public class MoveTo : Command
+    {
+        public MoveTo(Canvas drawingCanvas, string userInput)
+        {
+            Name = "moveto";
+            UserInput = userInput;
+            DrawingCanvas = drawingCanvas;
+        }
+
+        public void ParseParameters(string userInput)
+        {
+            string[] splitUserInput = userInput.Split(" ");
+
+            if (splitUserInput.Length == 3)
+            {
+                try
+                {
+                    ParameterList.Add(Int32.Parse(splitUserInput[1]));
+                    ParameterList.Add(Int32.Parse(splitUserInput[2]));
+                }
+                catch (ArgumentException e)
+                {
+                    throw new ArgumentException("Invalid parameters", e);
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Number of parameters is incorrect");
+            }
+        }
+
+        public override void Operation()
+        {
+            int[] parameters = ParameterList.ToArray();
+            drawingCanvas.PointX = parameters[0];
+            drawingCanvas.PointY = parameters[1];
+        }
+    }
+}
